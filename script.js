10c1452e299ae375aa3cce8c15018f04cb6d701f
Harshit Ranjan <137553967+harshitrnjn@users.noreply.github.com>
2023-08-27 21:55:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+gsap.to(".text1",{
+    x:-50,
+    y:-400,
+    duration:1.5,
+    ease:Expo.easeInOut
+})
+gsap.to(".text3",{
+    x:450,
+    y:-400,
+    duration:1.5,
+    ease:Expo.easeInOut
+})
+gsap.to(".text2",{
+    x:-300,
+    y:120,
+    duration:1.5,
+    ease:Expo.easeInOut
+})
+gsap.to(".text4",{
+    x:200,
+    y:120,
+    duration:1.5,
+    ease:Expo.easeInOut
+})
+gsap.from(".title",{
+    opacity:0,
+    delay:1.5,
+    duration:1.5
+})
+// gsap.from("#line",{
+//     height:"0%",
+//     duration:1,
+//     delay:.5,
+//     ease:Expo.easeInOut
+// })
+gsap.from(".img img",{
+    opacity:0,
+    scale:1.09,
+    duration:1.5,
+    delay:1.5,
+    ease:Expo.easeInOut
+})
+gsap.from(".left i",{
+    opacity:0,
+    x:-30,
+    duration:1.5,
+    delay:1.5,
+    ease:Expo.easeInOut
+})
+gsap.from(".left h1",{
+    opacity:0,
+    x:-30,
+    duration:1.5,
+    delay:1.2,
+    ease:Expo.easeInOut
+})
+gsap.from(".logo",{
+    opacity:0,
+    y:-30,
+    duration:1.5,
+    delay:1.5,
+    ease:Expo.easeInOut
+})
+gsap.from(".fb",{
+    opacity:0,
+    x:30,
+    duration:1.5,
+    delay:1.5,
+    ease:Expo.easeInOut
+})
+gsap.from(".snap",{
+    opacity:0,
+    x:30,
+    duration:1.5,
+    delay:1.7,
+    ease:Expo.easeInOut
+})
+gsap.from(".insta",{
+    opacity:0,
+    x:30,
+    duration:1.5,
+    delay:1.9,
+    ease:Expo.easeInOut
+})
+gsap.from(".btn",{
+    opacity:0,
+    duration:1.5,
+    delay:2.5,
+    ease:Expo.easeInOut
+})
